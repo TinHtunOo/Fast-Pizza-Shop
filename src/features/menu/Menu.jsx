89cd31e040093cf3cf4ae3efcf4e4a28fea0ff34
TinHtunOo <tinhtunhe@gmail.com>
2025-02-07 01:39:20 +0700

    first upload

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+function Menu() {
+  let menu = useLoaderData();
+  // console.log(menu);
+  return (
+    <div>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </div>
+  );
+}
+
+export async function loader() {
+  let menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
